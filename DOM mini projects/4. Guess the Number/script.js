79b867dadd685c2c6e492b8a5cac4667ae8d1251
Prianshu Sharma <prianshusharma993@gmail.com>
2024-05-26 03:48:25 +0530

    project4 updated

---
+++ DOM mini projects/4. Guess the Number/script.js
@@ -0,0 +1,43 @@
+let randomNumber = parseInt(Math.random()*100 + 1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuesses = [];
+let numGuesses = 1;
+
+let playGame = true;
+
+if(playgame){
+
+}
+
+function validateGuess(guess) {
+    //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayMessage(message){
+    //
+}
+
+function displayGuess (guess){
+    //
+}
+
+function endGame(){
+    //
+}
+
+function newGame(){
+    //
+}
